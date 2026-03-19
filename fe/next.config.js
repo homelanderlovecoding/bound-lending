258129ed774814b360c8ebff04c3768646c9388b
homelander <heocon161106@gmail.com>
2026-03-19 22:44:05 +0700

    fix: FE build fixes for Vercel - eslint v8, tsconfig test exclusions, .npmrc, vercel.json

--- fe/next.config.js
+++ fe/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude test files from production build
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(spec|test)\.(ts|tsx)$/,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
